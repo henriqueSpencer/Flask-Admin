bc85d7710dca15c6e00c903792a88c33fcc9812a
Unknown <henriquespencer_@hotmail.com>
2017-12-26 16:47:24 -0300

    fazendo

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import 
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisupposedtobesecret!'
+
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
